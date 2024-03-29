f8401ff48679e226f7b1de01a32f749da849f990
leesoyeon93 <soyeon@talktomeinkorean.com>
2024-03-29 18:30:01 +0900

    feat: :sparkles: moking 초기화

--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -12,10 +12,17 @@ beforeAll(() => {
 
 afterEach(() => {
   server.resetHandlers();
+
+  // 모킹된 모의 객체 호출에 대한 히스토리 초기화
+  // 모킹된 모듈의 구현을 초기화하진 않는다 -> 모킹된 상태로 유지
+  // 모킹 모듈 기반으로 작성된 테스트가 올바르게 실행
+  // 반면, 모킹 히스토리가 계속 쌓임 (호출 횟수나 인자가 계속 변경) => 다른 테스트에 영향을 줄 수 있음
+  // 따라서 clearAllMocks()를 호출하여 히스토리를 초기화함
   vi.clearAllMocks();
 });
 
 afterAll(() => {
+ // 모킹 모듈에 대한 모든 구현을 초기화한다.
   vi.resetAllMocks();
   server.close();
 });
